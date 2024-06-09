19fbe42e8eee739dad938de63621d33b8b1e0380
Taisuke Miyazaki <imishinist@gmail.com>
2024-06-10 01:11:18 +0900

    add multiple record with WriteBatch

--- main.cc
+++ main.cc
@@ -1,6 +1,17 @@
 #include <cassert>
+#include <cstdint>
+#include <vector>
 #include <leveldb/db.h>
 #include <leveldb/options.h>
+#include <leveldb/write_batch.h>
+
+void write_varint32(std::vector<uint8_t>& result, uint32_t value) {
+    while (value > 0x7F) {
+        result.push_back(static_cast<uint8_t>((value & 0x7F) | 0x80));
+        value >>= 7;
+    }
+    result.push_back(static_cast<uint8_t>(value));
+}
 
 int main() {
     std::string path = "/tmp/leveldb";
@@ -12,9 +23,19 @@ int main() {
     leveldb::Status status = leveldb::DB::Open(options, path, &db);
     assert(status.ok());
 
-    std::string key = "name";
-    std::string value = "LevelDB";
-    status = db->Put(leveldb::WriteOptions(), key, value);
+    leveldb::WriteBatch batch;
+    for (int i = 0; i < 1024; i++) {
+        std::vector<uint8_t> input;
+        write_varint32(input, i);
+        std::string key(input.begin(), input.end());
+        std::string value = std::to_string(i);
+
+        batch.Put(key, value);
+    }
+
+    leveldb::WriteOptions write_options;
+    write_options.sync = true;
+    status = db->Write(write_options, &batch);
     assert(status.ok());
 
     delete db;
