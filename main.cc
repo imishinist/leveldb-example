579ff66067a09fc6348b521ead81ca3348ea860f
Taisuke Miyazaki <imishinist@gmail.com>
2024-06-10 11:09:04 +0900

    read sample

--- main.cc
+++ main.cc
@@ -1,9 +1,26 @@
 #include <cassert>
 #include <cstdint>
+#include <iostream>
 #include <vector>
 #include <leveldb/db.h>
 #include <leveldb/options.h>
+#include <leveldb/slice.h>
 #include <leveldb/write_batch.h>
+
+void encode_to_hex(const leveldb::Slice input, std::string& output) {
+    output.clear();
+    output.push_back('[');
+    for (size_t i = 0; i < input.size(); i++) {
+        if (i > 0) {
+            output.push_back(' ');
+        }
+
+        const unsigned char b = static_cast<char>(input[i]);
+        output.push_back("0123456789ABCDEF"[b >> 4]);
+        output.push_back("0123456789ABCDEF"[b & 0x0F]);
+    }
+    output.push_back(']');
+}
 
 void write_varint32(std::vector<uint8_t>& result, uint32_t value) {
     while (value > 0x7F) {
@@ -13,16 +30,22 @@ void write_varint32(std::vector<uint8_t>& result, uint32_t value) {
     result.push_back(static_cast<uint8_t>(value));
 }
 
-int main() {
-    std::string path = "/tmp/leveldb";
+void read_all(leveldb::DB* db) {
+    leveldb::ReadOptions read_options;
+    read_options.verify_checksums = true;
+    leveldb::Iterator* iter = db->NewIterator(read_options);
+    iter->SeekToFirst();
+    while (iter->Valid()) {
+        std::string key, value;
+        encode_to_hex(iter->key(), key);
+        encode_to_hex(iter->value(), value);
+        std::cout << "key: " << key << ", value: " << value << "\n";
+        iter->Next();
+    }
+    delete iter;
+}
 
-    leveldb::DB *db;
-    leveldb::Options options;
-    options.create_if_missing = true;
-
-    leveldb::Status status = leveldb::DB::Open(options, path, &db);
-    assert(status.ok());
-
+void writes(leveldb::DB* db) {
     leveldb::WriteBatch batch;
     for (int i = 0; i < 1024; i++) {
         std::vector<uint8_t> input;
@@ -35,8 +58,22 @@ int main() {
 
     leveldb::WriteOptions write_options;
     write_options.sync = true;
-    status = db->Write(write_options, &batch);
+    leveldb::Status status = db->Write(write_options, &batch);
     assert(status.ok());
+}
+
+int main() {
+    std::string path = "/tmp/leveldb";
+
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+
+    leveldb::Status status = leveldb::DB::Open(options, path, &db);
+    assert(status.ok());
+
+    // writes(db);
+    read_all(db);
 
     delete db;
 
